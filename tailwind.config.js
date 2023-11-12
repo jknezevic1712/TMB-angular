e902254bff7287bd6871ff109fdcb62a2867a88a
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-11-12 20:56:00 +0100

    Initial commit with TailwindCSS setup.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2560px",
+      },
+    },
+  },
+  plugins: [],
+};
