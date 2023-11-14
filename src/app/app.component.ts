6280def6df13bb5b3f2bf7e60a792aa2e24bd6bc
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-11-14 14:32:51 +0100

    Changes:
    Added Layout component
    
    Adjusted global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutComponent } from './shared/components/organisms/layout/layout.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
+  styles: [':host { display: block; height: inherit; }'],
 })
 export class AppComponent {}
---
+++ src/app/shared/components/organisms/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent {}
