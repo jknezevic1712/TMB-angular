63378e8e2a03004044d8c93fb723798403ad31a6
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-11-15 11:15:08 +0100

    Changes:
    Implemented Button component from angular/material package.
    
    Working on implementing other atoms from angular/material.

--- src/app/shared/components/organisms/auth-actions/auth-actions.component.ts
+++ src/app/shared/components/organisms/auth-actions/auth-actions.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// components
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-auth-actions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './auth-actions.component.html',
 })
 export class AuthActionsComponent {
